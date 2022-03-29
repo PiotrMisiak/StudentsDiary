f2d00ff51d5a7abc9bb6c93ddbec5b740e666102
PiotrMisiak <piotrektrumpet@gmail.com>
2022-03-29 08:57:00 +0200

    Add project files.

---
+++ StudentsDiary/AddEditStudent.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace StudentsDiary
+{
+    partial class AddEditStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.lbId = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbMath = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTechnology = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbPhysics = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbPolishLang = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbForeignLang = new System.Windows.Forms.TextBox();
+            this.rtbComments = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.cbActivities = new System.Windows.Forms.CheckBox();
+            this.cbGroupId = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(106, 25);
+            this.tbId.Name = "tbId";
+            this.tbId.ReadOnly = true;
+            this.tbId.Size = new System.Drawing.Size(180, 22);
+            this.tbId.TabIndex = 0;
+            // 
+            // lbId
+            // 
+            this.lbId.AutoSize = true;
+            this.lbId.Location = new System.Drawing.Point(12, 28);
+            this.lbId.Name = "lbId";
+            this.lbId.Size = new System.Drawing.Size(23, 17);
+            this.lbId.TabIndex = 1;
+            this.lbId.Text = "Id:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Imię:";
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(106, 53);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(180, 22);
+            this.tbFirstName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nazwisko:";
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(106, 81);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(180, 22);
+            this.tbLastName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Matematyka:";
+            // 
+            // tbMath
+            // 
+            this.tbMath.Location = new System.Drawing.Point(106, 109);
+            this.tbMath.Name = "tbMath";
+            this.tbMath.Size = new System.Drawing.Size(180, 22);
+            this.tbMath.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Technologia:";
+            // 
+            // tbTechnology
+            // 
+            this.tbTechnology.Location = new System.Drawing.Point(106, 137);
+            this.tbTechnology.Name = "tbTechnology";
+            this.tbTechnology.Size = new System.Drawing.Size(180, 22);
+            this.tbTechnology.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Fizyka:";
+            // 
+            // tbPhysics
+            // 
+            this.tbPhysics.Location = new System.Drawing.Point(106, 165);
+            this.tbPhysics.Name = "tbPhysics";
+            this.tbPhysics.Size = new System.Drawing.Size(180, 22);
+            this.tbPhysics.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Jęź.pol.:";
+            // 
+            // tbPolishLang
+            // 
+            this.tbPolishLang.Location = new System.Drawing.Point(106, 193);
+            this.tbPolishLang.Name = "tbPolishLang";
+            this.tbPolishLang.Size = new System.Drawing.Size(180, 22);
+            this.tbPolishLang.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 224);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Jęź.obc.:";
+            // 
+            // tbForeignLang
+            // 
+            this.tbForeignLang.Location = new System.Drawing.Point(106, 221);
+            this.tbForeignLang.Name = "tbForeignLang";
+            this.tbForeignLang.Size = new System.Drawing.Size(180, 22);
+            this.tbForeignLang.TabIndex = 14;
+            // 
+            // rtbComments
+            // 
+            this.rtbComments.Location = new System.Drawing.Point(106, 307);
+            this.rtbComments.Name = "rtbComments";
+            this.rtbComments.Size = new System.Drawing.Size(180, 96);
+            this.rtbComments.TabIndex = 16;
+            this.rtbComments.Text = "";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(14, 280);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 17);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Uwagi:";
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(203, 409);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(83, 23);
+            this.btnConfirm.TabIndex = 18;
+            this.btnConfirm.Text = "Zatwierdź";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(106, 409);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(85, 23);
+            this.btnCancel.TabIndex = 19;
+            this.btnCancel.Text = "Anuluj";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // cbActivities
+            // 
+            this.cbActivities.AutoSize = true;
+            this.cbActivities.BackColor = System.Drawing.Color.Transparent;
+            this.cbActivities.Location = new System.Drawing.Point(106, 280);
+            this.cbActivities.Name = "cbActivities";
+            this.cbActivities.Size = new System.Drawing.Size(148, 21);
+            this.cbActivities.TabIndex = 20;
+            this.cbActivities.Text = "Zajęcia dodatkowe";
+            this.cbActivities.UseVisualStyleBackColor = false;
+            // 
+            // cbGroupId
+            // 
+            this.cbGroupId.FormattingEnabled = true;
+            this.cbGroupId.Items.AddRange(new object[] {
+            "Wszystkie",
+            "1A",
+            "1B",
+            "2A",
+            "2B"});
+            this.cbGroupId.Location = new System.Drawing.Point(106, 250);
+            this.cbGroupId.Name = "cbGroupId";
+            this.cbGroupId.Size = new System.Drawing.Size(180, 24);
+            this.cbGroupId.TabIndex = 21;
+
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 253);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 17);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Grupa:";
+            // 
+            // AddEditStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(306, 448);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cbGroupId);
+            this.Controls.Add(this.cbActivities);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.rtbComments);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbForeignLang);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbPolishLang);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbPhysics);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbTechnology);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbMath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.lbId);
+            this.Controls.Add(this.tbId);
+            this.MaximumSize = new System.Drawing.Size(324, 495);
+            this.MinimumSize = new System.Drawing.Size(324, 495);
+            this.Name = "AddEditStudent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dodawanie ucznia";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.Label lbId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbMath;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbTechnology;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbPhysics;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbPolishLang;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbForeignLang;
+        private System.Windows.Forms.RichTextBox rtbComments;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.CheckBox cbActivities;
+        private System.Windows.Forms.ComboBox cbGroupId;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ StudentsDiary/Main.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace StudentsDiary
+{
+    public partial class Main : Form
+    {        
+        private FileHelper<List<Student>> _fileHelper = 
+            new StudentsDiary.FileHelper<List<Student>>(Program.FilePath);
+
+        public Main()
+        {
+            InitializeComponent();
+            RefreshDiary();
+            SetColumnsHeader();
+            cbGroupSet.Text = "Wszystkie klasy";
+        }
+
+        private void RefreshDiary()
+        {
+            var students = _fileHelper.DeserializeFromFile().OrderBy(x => x.Id).ToList();
+            dgvDiary.DataSource = students;
+        }
+
+        private void SetColumnsHeader()
+        {
+            dgvDiary.Columns[0].HeaderText = "Numer";
+            dgvDiary.Columns[1].HeaderText = "Imię";
+            dgvDiary.Columns[2].HeaderText = "Nazwisko";
+            dgvDiary.Columns[3].HeaderText = "Uwagi";
+            dgvDiary.Columns[4].HeaderText = "Matematyka";
+            dgvDiary.Columns[5].HeaderText = "Technologia";
+            dgvDiary.Columns[6].HeaderText = "Fizyka";
+            dgvDiary.Columns[7].HeaderText = "Język polski";
+            dgvDiary.Columns[8].HeaderText = "Język obcy";
+            dgvDiary.Columns[9].HeaderText = "Zajęcia dodatkowe";
+            dgvDiary.Columns[9].ReadOnly = true;
+            dgvDiary.Columns[10].Visible = false;
+        }
+        
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var addEditstudent = new AddEditStudent();
+            addEditstudent.FormClosing += AddEditstudent_FormClosing;
+            addEditstudent.ShowDialog();
+        }
+
+        private void AddEditstudent_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RefreshDiary();          
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dgvDiary.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Prosze zaznacz ucznia, którego dane chcesz edytować.");
+                return;
+            }
+
+            var addEditstudent = new AddEditStudent(
+                Convert.ToInt32(dgvDiary.SelectedRows[0].Cells[0].Value));
+            addEditstudent.FormClosing += AddEditstudent_FormClosing;
+            addEditstudent.ShowDialog();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvDiary.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Prosze zaznacz ucznia, którego chcesz usunąć.");
+                return;
+            }
+
+            var selectedStudent = dgvDiary.SelectedRows[0];
+
+            var confirmDelete = 
+                MessageBox.Show($"Czy na pewno chcesz usunąc ucznia {(selectedStudent.Cells[1].Value.ToString() + " " + selectedStudent.Cells[2].Value.ToString()).Trim()}", 
+                "Usuwanie ucznia", 
+                MessageBoxButtons.OKCancel);
+
+            if (confirmDelete == DialogResult.OK)
+            {
+                DeleteStudent(Convert.ToInt32(selectedStudent.Cells[0].Value));               
+                RefreshDiary();
+            }
+        }
+
+        private void DeleteStudent(int id)
+        {
+            var students = _fileHelper.DeserializeFromFile().OrderBy(x => x.Id).ToList();
+            students.RemoveAll(x => x.Id == id);
+            _fileHelper.SerializeToFile(students);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            var students = _fileHelper.DeserializeFromFile();
+            var selectedGroup = cbGroupSet.SelectedItem.ToString();
+            
+            if (selectedGroup == "Klasa 1A")
+            
+                dgvDiary.DataSource = students.FindAll(x => x.GroupId == "1A");
+
+            else if (selectedGroup == "Klasa 1B")
+
+                dgvDiary.DataSource = students.FindAll(x => x.GroupId == "1B");
+           
+            else if (selectedGroup == "Klasa 2A")
+
+                dgvDiary.DataSource = students.FindAll(x => x.GroupId == "2A");
+           
+            else if (selectedGroup == "Klasa 2B")
+
+                dgvDiary.DataSource = students.FindAll(x => x.GroupId == "2B");
+            
+            else
+
+
+            RefreshDiary();
+        }
+    }
+}
